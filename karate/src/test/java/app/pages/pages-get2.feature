55880eff5c83a321119c4d7699fb8d3157e789db
rajagopal <veearegee@gmail.com>
2020-06-20 16:37:12 +0530

    delete parent folder

--- karate/src/test/java/app/pages/pages-get2.feature
+++
@@ -1,9 +0,0 @@
-Feature: API test with Karate DSL
-
-Scenario: Verify return of pages
-
-Given url 'http://dummy.restapiexample.com/api/v1/employees'
-When method get
-Then status 200
-
-* print 'Tester', a
--- karate/src/test/java/karate-config.js
+++
@@ -1,9 +0,0 @@
- function() {   
-  var config = {
-    apiURL: 'http://35.188.114.237:8088'
-  };
-  
-  karate.configure('connectTimeout', 50000);
-  karate.configure('readTimeout', 5000);
-  return config;
-}
